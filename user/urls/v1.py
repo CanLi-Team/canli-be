4e59bd4dd471e9741f47e0f137c123be8fe5dfee
Darsh <darsh-008>
2023-07-17 18:46:15 -0400

    creating api for fetching user details

--- user/urls/v1.py
+++ user/urls/v1.py
@@ -12,7 +12,6 @@ profile_create = views.UserViewSet.as_view({
 urlpatterns = [
 	path('', include(router.urls)),
 	path('generate_otp', views.GenerateOTP.as_view(), name='generate-otp'),
-    path('re_generate_otp', views.GenerateOTP.as_view(), name='re-generate-otp'),
     path('verify_otp', views.VerifyOTP.as_view(), name='verify-otp'),
     path('add/feedback', views.UserFeedbackAPIView.as_view(), name='user-feedback'),
     path('add/test_date', views.AddTestDate.as_view(), name='add-testdate'),
