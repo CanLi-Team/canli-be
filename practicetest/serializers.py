b33955beb153e51051b222033ba4b65b31e7894c
Darsh <darsh-008>
2023-07-18 16:35:28 -0400

    added code for challenge bank in serialize file

--- practicetest/serializers.py
+++ practicetest/serializers.py
@@ -8,9 +8,17 @@ class PracticeTestSerializer(ModelSerializer):
 	""" User Basic Information Serializer """
 	content = serializers.SerializerMethodField()
 
-	class Meta:
+	is_challanged = serializers.SerializerMethodField()
+
+class Meta:
 		model = PracticeTest
 		fields = ('id','test_type','question_type','question','option','answer','content','created_at','updated_at', 'is_bookmarked', 'is_challanged')
 
 def get_content(self, obj):
 		return UploadContentSerializer(obj.content).data
+
+def get_is_challanged(self, obj):
+        user_id = self.context['user_id']
+        user_practice = UserPractice.objects.filter(user__id = user_id, practice_test = obj)
+        if user_practice:
+            return user_practice.last().is_challanged
