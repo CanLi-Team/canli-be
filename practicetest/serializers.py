6c99ae56354d18ffef23e2eef69a0378d9bc9fc3
Karan <karan.dhanwani77@gmail.com>
2023-07-18 02:05:16 -0400

    Added code to serialize file for practice test

--- practicetest/serializers.py
+++ practicetest/serializers.py
@@ -1,2 +1,13 @@
 from rest_framework.serializers import (ModelSerializer,)
 from rest_framework import serializers
+from content.serializers import UploadContentSerializer
+from practicetest.models import *
+from user.models import User
+
+class PracticeTestSerializer(ModelSerializer):
+	""" User Basic Information Serializer """
+	content = serializers.SerializerMethodField()
+
+	class Meta:
+		model = PracticeTest
+		fields = ('id','test_type','question_type','question','option','answer','content','created_at','updated_at', 'is_bookmarked', 'is_challanged')
